141842084e8001aa9364d44a6e388675755fc33c
DevOpsCloudAutomation <123757746+DevOpsCloudAutomation@users.noreply.github.com>
2024-01-16 22:18:27 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pull Docker Image from DockerHub
-docker pull devopscloudautomation/javadockerapplication:74
+docker pull devopscloudautomation/javadockerapplication:76
 
 #Run Docker Image as a Container
-docker run -d --name javadockercontainer -p 8080:8080 devopscloudautomation/javadockerapplication:74
+docker run -d --name javadockercontainer -p 8080:8080 devopscloudautomation/javadockerapplication:76
