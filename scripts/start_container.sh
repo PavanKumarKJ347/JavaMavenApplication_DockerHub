bf612fb664b81b867b4a957ef22c5bddf106f418
Pavan Kumar K J <Pavankumarkj347@Gmail.com>
2024-01-24 14:30:45 +0530

    Updated start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pull Docker Image from DockerHub
-docker pull devopscloudautomation/javadockerapplication:117
+docker pull devopscloudautomation/javadockerapplication:120
 
 #Run Docker Image as a Container
-docker run -d --name javadockercontainer -p 8080:8080 devopscloudautomation/javadockerapplication:117
+docker run -d --name javadockercontainer -p 8080:8080 devopscloudautomation/javadockerapplication:120
