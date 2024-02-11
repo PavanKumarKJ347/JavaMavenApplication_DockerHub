5b560a89b4e668081ed3d53ac684341150fb40e8
DevOpsCloudAutomation <Pavankumarkj347@Gmail.com>
2024-02-11 16:53:39 +0530

    Updated Project Code

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-#Pull Docker Image from DockerHub
-docker pull devopscloudautomation/javadockerapplication:121
-
-#Run Docker Image as a Container
-docker run -d --name javadockercontainer -p 8080:8080 devopscloudautomation/javadockerapplication:121
--- scripts/stop_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-#Stop Running Container
-docker rm -f javadockercontainer
